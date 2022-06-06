4b81ee551260a3334fd8e225769d9a4963ec7950
ATsvetkov <tsvetvl@mail.ru>
2022-06-06 17:10:14 +1000

    Условия

--- vba01.vb
+++ vba01.vb
@@ -1,3 +1,5 @@
+'1.1
+
 Global Const gKolRec As Integer = 1000   'const
 Global gKolMag As Integer
 Global gListMag() As String        'array
@@ -9,8 +11,62 @@ Dim mEndMagRow As Integer
 Dim mAdr As String   
 Dim mMagInList As Boolean  
 Dim mMagName As String 
-Dim mCena As Single       'простое дробное
+Dim mCena As Single       
     
 mMagInList = False
 mMagName = ""
-mItogListName = "Total"   
+mItogListName = "Total"  
+
+'---
+'2.1 
+
+If mSprMagListName <> "" Then
+   Worksheets(Trim(mSprMagListName)).Select
+End If
+
+
+'2.2 
+
+If Cells(i, 4).Value <> Empty Then
+        '--
+End If
+
+
+'2.3
+
+If mRetVal = vbNo Then
+    Exit Sub      
+End If
+
+'2.4
+
+If Trim(mTovName) = Trim(Cells(i, 2).Value) Then
+  pGetMagTovRow = i
+  Exit Function
+End If
+
+'2.5
+
+If mTovInMagRow < 2 Then
+    '--
+End If
+
+'2.6
+If Cells(mStartRow - 1, i) = Empty And Cells(mStartRow - 2, i) = Empty Then
+    '--
+End If
+
+'2.7
+
+If Left(Trim(Cells(mEndRow + 2, 2).Value), 5) = "Total" And Left(Trim(Cells(mEndRow + 3, 2).Value), 5) = "Total" Then
+    MsgBox ("...")
+    Exit Sub
+End If
+
+'2.8
+  
+If Left(Trim(Cells(x, mIDCol)), 2) = "ds" Then
+    '-- 
+End If
+                
+'---         
