aecef6857d30888cca1a3761604e94fb37a09981
ATsvetkov <tsvetvl@mail.ru>
2022-06-06 16:55:27 +1000

    Переменные

---
+++ vba01.vb
@@ -0,0 +1,16 @@
+Global Const gKolRec As Integer = 1000   'const
+Global gKolMag As Integer
+Global gListMag() As String        'array
+Global gListName() As String
+
+
+Dim mStartMagRow As Integer
+Dim mEndMagRow As Integer   
+Dim mAdr As String   
+Dim mMagInList As Boolean  
+Dim mMagName As String 
+Dim mCena As Single       'простое дробное
+    
+mMagInList = False
+mMagName = ""
+mItogListName = "Total"   
